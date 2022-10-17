ea9b86b8293fa25350f6aae3e2cbfb3f30b55e86
Nikita Malukov <nikita_malukov@epam.com>
2022-10-18 00:17:39 +0200

    Add project files.

---
+++ SimpleChat.Client/MessageSource.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace SimpleChat.Client;
+
+public static class MessageSource
+{
+    private static string[] messageSource;
+    private static Random random = new(int.Parse(string.Join(string.Empty, Guid.NewGuid().ToString().Select(char.IsNumber))));
+
+    public static IEnumerable<string> GetMessages(int count = 10, int length = 5)
+    {
+        if (count < 0) throw new ArgumentException("Messages count can't be negative");
+        if (length < 0) throw new ArgumentException("Messages length can't be negative");
+
+        return MessagesGenerator(count, length);
+    }
+
+    private static IEnumerable<string> MessagesGenerator(int count, int length)
+    {
+        var builder = new StringBuilder(length);
+
+        while(count-- > 0)
+        {
+            builder.Clear();
+
+            for (int index = 0; index < length; index++)
+            {
+                builder.Append(messageSource[random.Next(0, messageSource.Length)] + " ");
+            }
+
+            yield return builder.ToString().Trim();
+        }
+    }
+
+    static MessageSource()
+    {
+        messageSource = ("Lorem ipsum dolor sit amet, " +
+            "consectetur adipiscing elit. Pellentesque bibendum" +
+            " dui sed odio semper, id luctus metus facilisis. " +
+            "Quisque vel ante sed diam varius ultrices in tristique nisl. " +
+            "Proin egestas risus dapibus porttitor facilisis. " +
+            "Ut tincidunt ultricies sapien, at dapibus lacus laoreet a. " +
+            "Proin eget justo in ipsum sagittis vulputate non et justo. " +
+            "Nulla pellentesque nulla sed ex vulputate interdum. " +
+            "Fusce tempus fringilla nisl ac egestas. Sed vel bibendum nibh. " +
+            "Duis auctor augue augue, ac commodo arcu luctus quis. " +
+            "Curabitur at semper erat, malesuada tincidunt nibh. " +
+            "Phasellus lacus odio, vestibulum id massa vitae, " +
+            "venenatis suscipit eros. Nullam imperdiet tempus est.").
+            ToLowerInvariant()
+            .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    }
+}
---
+++ SimpleChat.Client/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.IO.Pipes;
+using System.Text;
+using SimpleChat.Client;
+
+const int ReadDelay = 500;
+var cts = new CancellationTokenSource();
+var random = new Random();
+
+Console.WriteLine("Welcome to simple chat, please enter your name: ");
+var name = Console.ReadLine();
+
+Console.WriteLine("Please enter message count: ");
+var messageCount = int.Parse(Console.ReadLine() ?? "10");
+
+Console.WriteLine("Please enter message length: ");
+var messageLength = int.Parse(Console.ReadLine() ?? "5" );
+
+Console.WriteLine("Press ESC to cancel");
+var cancellationTask = ReadForCancellation();
+
+var chatServerStream = new NamedPipeClientStream("SimpleChat.Server", "MainPipe", PipeDirection.InOut, PipeOptions.Asynchronous);
+
+try
+{
+    Console.WriteLine("Connecting to server...");
+    await chatServerStream.ConnectAsync(cts.Token);
+
+    Console.WriteLine($"Result: {(chatServerStream.IsConnected ? "connected" : "disconnected")}");
+
+    if (!chatServerStream.IsConnected) return;
+
+    await Task.Run(WriteMessages, cts.Token);
+    var readerTask = Task.Run(ReadMessages, cts.Token);
+
+    Task.WaitAny(cancellationTask, readerTask);
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Operation was cancelled gracefully");
+}
+catch (Exception e)
+{
+    Console.BackgroundColor = ConsoleColor.DarkRed;
+    Console.ForegroundColor = ConsoleColor.White;
+
+    Console.WriteLine("Unexpected exception has occurred...");
+    Console.WriteLine($"Exception message: {e.Message}");
+
+    Console.ResetColor();
+}
+finally
+{
+    await CloseServerConnection();
+}
+
+async Task CloseServerConnection()
+{
+    Console.WriteLine("Disconnecting from the server...");
+    await chatServerStream!.DisposeAsync();
+    Console.WriteLine("Disconnected");
+}
+
+
+async Task WriteMessages()
+{
+    foreach (var generatedMessage in MessageSource.GetMessages(messageCount, messageLength))
+    {
+        cts.Token.ThrowIfCancellationRequested();
+        if (!chatServerStream.IsConnected) return;
+
+        var messageWithOverhead = AddMessageOverhead(generatedMessage);
+        var byteMessage = Encoding.Default.GetBytes(messageWithOverhead);
+        Console.WriteLine($"Sending \"{messageWithOverhead}\" to server...");
+        await chatServerStream.WriteAsync(byteMessage, cts.Token);
+        Console.WriteLine("Message was delivered");
+        Thread.Sleep(random.Next(1000, 3000));
+    }
+}
+
+async Task ReadMessages()
+{
+    var chatStreamReader = new StreamReader(chatServerStream, Encoding.Default);
+    var serverMessage = string.Empty;
+
+    while (!cts.IsCancellationRequested && chatServerStream.IsConnected)
+    {
+        cts.Token.ThrowIfCancellationRequested();
+
+        if (chatStreamReader.EndOfStream)
+        {
+            Thread.Sleep(ReadDelay);
+            continue;
+        }
+
+        serverMessage = await chatStreamReader.ReadLineAsync();
+
+        Console.WriteLine("Message was received...");
+        Console.WriteLine($"Message content:\n{serverMessage}");
+    }
+}
+
+Task ReadForCancellation() => Task.Run(() =>
+{
+    while (!cts.IsCancellationRequested)
+    {
+        var keyInfo = Console.ReadKey(true);
+
+        if (keyInfo.Key != ConsoleKey.Escape) continue;
+
+        Console.WriteLine("Execute cancellation...");
+        cts.Cancel();
+    }
+});
+
+string AddMessageOverhead(string message) => $"{DateTime.Now} | {name} | {message}";
